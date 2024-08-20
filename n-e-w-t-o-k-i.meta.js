5d03ce3e392d828239d8d4dbfe485b5ab19eb39c
PINIcesISp <164283999+PINIcesISp@users.noreply.github.com>
2024-08-20 19:01:46 +0700

    up

--- n-e-w-t-o-k-i.meta.js
+++ n-e-w-t-o-k-i.meta.js
@@ -1,13 +1,12 @@
 // ==UserScript==
 // @name         n-e-w-t-o-k-i
 // @namespace    https://github.com/PINIcesISp/minh_voi_ta/raw/main/n-e-w-t-o-k-i.user.js
-// @version      2024.07.23
+// @version      2024.08.20
 // @description  down n-e-w-t-o-k-i
 // @author       You
 // @include      /^https?://newtoki\d+\.com/webtoon/*/*/
-// @icon         https://www.google.com/s2/favicons?sz=64&domain=newtoki346.com
-// @grant        GM_download
-// @grant        GM_info
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=newtoki349.com
+// @require      https://cdn.jsdelivr.net/npm/@zip.js/zip.js@2.7.48/dist/zip.min.js
 // @downloadURL  https://github.com/PINIcesISp/minh_voi_ta/raw/main/n-e-w-t-o-k-i.user.js
 // @updateURL    https://github.com/PINIcesISp/minh_voi_ta/raw/main/n-e-w-t-o-k-i.meta.js
 // ==/UserScript==
